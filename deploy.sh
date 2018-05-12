c9df7cb13a7a0500ed6aa3f68e125a55e0c6cbe1
Josh Nygaard <josh.nygaard@gmail.com>
2018-05-11 20:36:57 -0400

    rebuilding site Fri, 11 May, 2018  8:36:57 PM

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t hugo-academic # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-academic # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Go To Public folder
 cd public
 # Add changes to git.
-git add .
+git add -A
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -20,5 +20,28 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
+# Come Back
+cd ..#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
 cd ..
